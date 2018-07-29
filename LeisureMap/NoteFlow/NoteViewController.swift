382ca0509b33dae6455881021d920a977b0bf4c7
abcedu18 <1450330214@qq.com>
2018-07-29 11:25:04 +0800

    修改

--- LeisureMap/NoteFlow/NoteViewController.swift
+++ LeisureMap/NoteFlow/NoteViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class NoteViewController: UIViewController {
+class NoteViewController: UIViewController,UIImagePickerControllerDelegate{
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
+  
+    @IBAction func btnPhotoClick(_ sender: Any) {
+      
+        func camera()
+        {
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                let myPickerController = UIImagePickerController()
+                myPickerController.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate ;
+                myPickerController.sourceType = .camera
+                self.present(myPickerController, animated: true, completion: nil)
+            }
+            
+        }
+        
+        func photoLibrary()
+        {
+            
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                let myPickerController = UIImagePickerController()
+                myPickerController.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate ;
+                myPickerController.sourceType = .photoLibrary
+                self.present(myPickerController, animated: true, completion: nil)
+            }
+        }
+    }
+    
     
 
     /*
